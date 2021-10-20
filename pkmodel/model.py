09b8039d73d77bee63caa41f095d96c56e1e93fb
rsqgtms <qianhy1996@outlook.com>
2021-10-20 11:38:13 +0100

    test push

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -5,7 +5,13 @@
 class Model:
     """A Pharmokinetic (PK) model
 
-    Parameters
+    Parameters: 
+    'name': name of the model,
+    'Q_p1': the transition rate between central compartment and peripheral compartment,
+    'V_c': the volume of the central compartment,
+    'V_p1': the volume of the first peripheral compartment,
+    'CL': the clearance/elimination rate from the central compartment,
+    'X': the dose function,
     ----------
 
     value: numeric, optional
