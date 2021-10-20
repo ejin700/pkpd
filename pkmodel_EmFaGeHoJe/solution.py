cce9b460d1127ddf5ba4a17126ba77dbd960b9d8
QianHY <qianhy1996@outlook.com>
2021-10-20 20:08:04 +0100

    #25 generalise the rhs function for IV case

--- pkmodel_EmFaGeHoJe/solution.py
+++ pkmodel_EmFaGeHoJe/solution.py
@@ -40,18 +40,22 @@ class Solution:
         self.Model = Model()
 
         if model == 'IV':
-            self.y0 = np.array([0.0, 0.0])
             for parameter in parameters:
                 args = [
-                    parameter['Q_p1'], parameter['V_c'], parameter['V_p1'], parameter['CL'], parameter['X']
+                    parameter['Q_p'], parameter['V_c'], parameter['V_p'], parameter['CL'], parameter['X'], parameter['N']
                 ]
+                self.y0 = np.zeros(parameter['N']+1)
                 sol = scipy.integrate.solve_ivp(
                     fun=lambda t, y: self.Model.get_rhs_IV(t, y, *args),
                     t_span=[self.t_eval[0], self.t_eval[-1]],
                     y0=self.y0, t_eval=self.t_eval
                 )
                 plt.plot(sol.t, sol.y[0, :], label=parameter['name'] + '- q_c')
-                plt.plot(sol.t, sol.y[1, :], label=parameter['name'] + '- q_p1')
+                for i in range(1,parameter['N']+1):
+                    plt.plot(sol.t, sol.y[i, :], label=parameter['name'] + f'- q_p{i}')
+
+                
+                
 
         elif model == 'sub':
             self.y0 = np.array([0.0, 0.0, 0.0])
