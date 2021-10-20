2045797cdc5f7f8e94d4256843bb2148ec8456ef
rsqgtms <qianhy1996@outlook.com>
2021-10-20 14:37:55 +0100

    #5 added basic methods into the model.py file

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -7,11 +7,12 @@ class Model:
 
     Parameters: 
     'name': name of the model,
-    'Q_p1': the transition rate between central compartment and peripheral compartment,
-    'V_c': the volume of the central compartment,
-    'V_p1': the volume of the first peripheral compartment,
-    'CL': the clearance/elimination rate from the central compartment,
-    'X': the dose function,
+    :param Q_p1: float, the transition rate constant between the central compartment and 
+    the first peripheral compartment, [mL/h]
+    :param V_c: float, the volume of the central compartment, [mL]
+    :param V_p1: float, the volume of the first peripheral compartment, [mL]
+    :param CL: float, the clearance/elimination rate from the central compartment, [mL/h]
+    :param dose: the dose function,
     ----------
 
     value: numeric, optional
@@ -19,9 +20,36 @@ class Model:
     'V_c': numeric,
     'V_p1': numeric,
     'CL': numeric,
-    'X': function,
+    'dose': function,
 
     """
-    def __init__(self, value=42):
-        self.value = value
+    def __init__(self, name):
+        self.name = name
+
+    def add_parameters_IV(self, Q_p1, V_c, V_p1, CL, dose):
+        """"""
+        dict_parameter = {
+            'Q_p1' : Q_p1,
+            'V_c' : V_c,
+            'V_p1' : V_p1,
+            'CL' : CL,
+            'dose' : dose
+        }
+        self.param = dict_parameter
+        return dict_parameter
+
+    def get_rhs_IV(self,t,y):
+        """
+    :returns: list, first entry (float) is the rate of change of the quantity of the drug 
+    in the central compartment with respect to (wrt) time [ng/h]. Likewise, the second 
+    entry (float) is the rate of change wrt time of the quantity of the drug in the first 
+    peripheral compartment [ng/h]
+        """
+        q_c, q_p1 = y
+        transition = self.param['Q_p1'] * (q_c / self.param['V_c'] - q_p1 / self.param['V_p1'])
+        dqc_dt = self.param['dose'](t) - q_c / self.param['V_c'] * self.param['CL'] - transition
+        dqp1_dt = transition
+        return [dqc_dt, dqp1_dt]
+
+
 
