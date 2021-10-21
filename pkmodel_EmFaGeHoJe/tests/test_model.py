6499e4918b9f9f39db0b94ab8c46cc0a507be2fd
Jesse Murray <jessebmurray1@gmail.com>
2021-10-21 14:13:06 +0100

    add model unit test

--- pkmodel_EmFaGeHoJe/tests/test_model.py
+++ pkmodel_EmFaGeHoJe/tests/test_model.py
@@ -1,15 +1,30 @@
 import unittest
-import pkmodel as pk
+
+import pkmodel_EmFaGeHoJe.model as pkm
+import numpy as np
 
 
 class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
     """
-    def test_create(self):
+    def test_dose_constant(self):
         """
         Tests Model creation.
         """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+        t_eval = np.linspace(0, 1, 1000, endpoint=False)
+
+        X = 1.0
+        PKmodel = pkm.Model()
+
+        total_dose = 0
+        for t in t_eval:
+            total_dose += PKmodel.dose_constant(t=t, X=X)
+        
+        self.assertEqual(total_dose, 1000 * X)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
 
